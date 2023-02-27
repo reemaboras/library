874c0608cd93f2ca4fe7cf7138c3cf061e3f1ef0
aamal-darkal <aamal.darkal@gmail.com>
2023-02-27 15:10:37 +0300

    first migration

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\BookController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// Route::resource('categories' , CategoryController::class)->except('destroy' , 'show');
+// Route::resource('books' , BookController::class)->only('index' , 'show');
+
+Route::resource('categories' , CategoryController::class);
+Route::resource('books' , BookController::class);
